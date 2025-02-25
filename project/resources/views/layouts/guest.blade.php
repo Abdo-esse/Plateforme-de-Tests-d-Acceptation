07e1598225186313de823d4ceb7cc7f68013c32c
abdelilah2005 <abdelilahessemlali@gmail.com>
2025-02-25 21:40:50 +0000

    J'ai termin la l'instalation de breze

--- project/resources/views/layouts/guest.blade.php
+++ project/resources/views/layouts/guest.blade.php
@@ -6,7 +6,12 @@
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
         <title>{{ config('app.name', 'Laravel') }}</title>
-
+        <script src="https://unpkg.com/@tailwindcss/browser@4"></script>
+    <style type="text/tailwindcss">
+      @theme {
+        --color-clifford: #da373d;
+      }
+    </style>
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
