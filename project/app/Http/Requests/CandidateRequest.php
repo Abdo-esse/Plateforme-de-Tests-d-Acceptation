a24d379f47243b141f90b98a1e896c897cf0b7e4
abdelilah2005 <abdelilahessemlali@gmail.com>
2025-02-27 10:48:30 +0000

    J'ai termin candidate requeste

---
+++ project/app/Http/Requests/CandidateRequest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class CandidateRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            
+                'user_id' => 'required|exists:users,id',
+                'campus' => 'required|in:youssoufia,safi,nador',
+                'adress' => 'required|string|max:255',
+                'tele' => 'required|string|regex:/^(?:\+212|0)[5-7][0-9]{8}$/',
+                'cart_Identite' => 'required|image|mimes:jpeg,png,pdf|max:2048|unique:candidates,cart_Identite',
+                'datenaissance' => 'required|date|before:today|after:18 years ago'
+        ];
+    }
+}
--- project/app/Models/User.php
+++ project/app/Models/User.php
@@ -52,4 +52,10 @@ class User extends Authenticatable
     return $this->roles()->where('name', $role)->exists();
    }
 
+   public function candidates()
+    {
+        return $this->hasMany(Candidate::class);
+    }
+
+
 }
